4254419ae86e55e260582b1420dbb3167ee97bb1
Fabijan Bajo <bajo09@gmail.com>
2017-05-23 17:44:54 +0200

    Add extract module and handle file requests

---
+++ extract.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+var extractFilePath = function(url) {
+    var filePath;
+    var fileName = 'index.html';
+
+    if (url.length > 1) {
+        fileName = url.substring(1);
+    }
+    console.log('Filename: ' + fileName);
+
+    return path.resolve(__dirname, 'app', fileName);
+};
+
+// Make available to that other modules can import with require
+module.exports = extractFilePath;
+
+// Usage: var filePath = extract(request.url);
--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-var http = require('http');
+var http = require('http'); // Handle HTTP requests
+var fs = require('fs'); // Read local files
+var extract = require('./extract'); // handling and transforming file paths
 
+// Error handler
+var handleError = function(error, response) {
+    response.writeHead(404);
+    response.end('<H1>Sorry, page not found</H1>');
+};
+
+// Each request triggers the following callback
 var server = http.createServer(function(request, response) {
-    console.log('Responding to a request');
-    response.end('<h1>Hello, World</h1>');
+    // Construct filepath for local requested file
+    var filePath = extract(request.url);
+
+    // Read local file and send response to browser
+    fs.readFile(filePath, function(error, data) {
+        if (error) {
+            handleError(error, response);
+            return;
+        } else {
+            response.setHeader('Content-Type', 'text/html');
+            response.end(data);
+        }
+    });
 });
 
 // Binding to port 3000
