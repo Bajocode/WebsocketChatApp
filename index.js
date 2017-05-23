a9bc9003fec7c6005c9b41f14a83525074461616
Fabijan Bajo <bajo09@gmail.com>
2017-05-23 17:03:32 +0200

    Add nodemon package

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ var server = http.createServer(function(request, response) {
     console.log('Responding to a request');
     response.end('<h1>Hello, World</h1>');
 });
+
+// Binding to port 3000
 server.listen(3000);
