4c235b61272a87355dbdc54292060392a57cefa7
Fabijan Bajo <bajo09@gmail.com>
2017-05-25 14:07:39 +0200

    Add basis chatForm flow

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,16 +1,33 @@
 /*
     define the structure of messages and pass messages between and dom
+
+    Flow: sending outgoing chat messages
+    1. User submits message by typing and clicking on submit
+    2. ChatForm instance takes the data and sends to chatApp's callback
+    3. Callback will package it up as ChatMessage + send to WebSocket server
+        - ws-client makes sure ChatMessage is processable for socket protocol
+
+    FLow: display new messages from server
 */
 import socket from './ws-client';
+import { ChatForm } from './dom'; // {named import}
+
+const FORM_SELECTOR = '[data-chat="chat-form"]';
+const INPUT_SELECTOR = '[data-chat="message-input"]';
 
 // ES6 class (constructor sorthand syntax)
 class ChatApp {
     constructor() {
+        this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+
         socket.init('ws://localhost:3001');
-        // When connected, send pow!
+        // Open socket connection to server
         socket.registerOpenHandler(() => {
-            let message = new ChatMessage({ message: 'message!'});
-            socket.sendMessage(message.serialize());
+            // Initialize ChatForm instance with form submission callback
+            this.chatForm.init((data) => {
+                let message = ChatMessage(data);
+                socket.sendMessage(message.serialize());
+            });
         });
         // Forwarded message
         socket.registerMessageHandler((data) => {
--- app/scripts/src/dom.js
+++ app/scripts/src/dom.js
@@ -1,3 +1,46 @@
 /*
     display data to the UI and handle form submissions
+    - export default: export single value from module
+    - named exports: export muliple named values
 */
+
+//  jQuery for DOM manipulation
+import $ from 'jquery'
+
+// Chatform constructor for accepting selctors (client sending out messages)
+export class ChatForm {
+    constructor(formSelector, inputSelector) {
+        this.$form = $(formSelector);
+        this.$input = $(inputSelector);
+    }
+
+    // Initialize with callback from form submit event
+    // Handle form submissions from UI through ws-client (socket client side)
+    init(submitCallback) {
+        this.$form.submit((event)) => {
+            event.preventDefault();
+            // Retrieve value from inputfield
+            let val = this.$input.val();
+            // Pass value to submitcallback
+            submitCallback(val);
+            this.$input.val(' ');
+        });
+        // Add click handler to fire submit event (single expression arrow)
+        this.$form.find('button').on('click', () => this.$form.submit());
+    }
+}
+
+// Create DOM elements for each message
+export class ChatList {
+    constructor(listSelector, userName) {
+        // Know where to attach message list elements
+        this.$list = $(listSelector);
+        // Know which message sent by user and everybody else
+        this.username = userName;
+    }
+
+    // Create a row for message with message contents / data
+    drawMessage({user: u, timestamp: t, message: m}) {
+
+    }
+}
